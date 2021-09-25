0694330a782e6410aa3f15820e75205a77a58867
sy <1134119490@qq.com>
2021-09-25 09:08:43 +0800

    vue3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App 组件</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      
+      }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixins.styl"
+  h1 
+    width 375px
+    background red
+  
+
+</style>
